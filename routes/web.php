3c5334fa6a849b591d6af7e66e5d4652d234d8a9
Mikhail Podshivalov <mapodshivalov@gmail.com>
2021-12-31 13:10:53 +0300

    connected the frontend

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Example Routes
+Route::view('/', 'landing');
+Route::match(['get', 'post'], '/dashboard', function(){
+    return view('dashboard');
 });
+Route::view('/pages/datatables', 'pages.datatables');
+
+//Demo:
+//https://demo.pixelcave.com/dashmix/be_layout_content_main_full_width.html
